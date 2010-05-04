03b59d94e2c7af03dda9616ef2058d9f900ba601
Nicholas Cain <nicain.seattle@gmail.com>
2010-05-03 18:46:43 -0700

    Major change; need settings file to run, not part of repository

--- DDMCube_Master.py
+++ DDMCube_Master.py
@@ -16,57 +16,21 @@ from os.path import join as join, abspath as abspath
 import scipy, random, time, os, pickle, uuid
 import analysisTools as at
 import pbsTools as pt
-DDMCube_cythonDevDir = '/Users/Nick/Library/Python/src/DDMCube_cython'
 
 # Compile cython extension DDMCube.pyx:
 call('python setup.py build_ext --inplace', shell=True)
 
 ################################################################################
-# Dashboard:
-################################################################################
-
-# Define job settings:
-settings={									# Example values:
-'A':list(scipy.linspace(0,0,1)),			# 0
-'B':list(scipy.linspace(0,0,1)),			# 0
-'beta':list(scipy.linspace(0,.1,1)),		# 0
-'chopHat':list(scipy.linspace(0,2,1)),			# 0
-'dt':list(scipy.linspace(.1,.1,1)),		# .02
-'K':list(scipy.linspace(9,10.5,1)),		# 20
-'tMax':list(scipy.linspace(2000,1000,1)),	# 2000, or 400->600 in FD paradigm
-'theta':list(scipy.linspace(1,100,2)),		# 10
-'C':[6.4],#(3.2*scipy.concatenate([[0],2**scipy.linspace(0,4,5)])),  #[0,3.2,6.4,12.8,25.6,51.2],#list(scipy.linspace(0,20,7)),		# Now units of C
-'xStd':list(scipy.linspace(0,15,1)),		# 12.8; 0 means set in DDMCube as 4.46*Mean
-'xTau':list(scipy.linspace(20,25,1)),		# 20
-'yBegin':list(scipy.linspace(0,40,1)),		# 40
-'yTau':list(scipy.linspace(20,10,1)),
-'noiseSigma':list(scipy.linspace(14,15,2))		# 10
-}
-
-# Define job parameters:
-quickName = 'debug'
-FD=0
-
-# Define type of job
-dryRun = 0
-localRun = 1						# Superceded by dryRun
-server = 'wallTimeEstimate'			# 'normal' or 'wallTimeEstimate'
-
-# Divide jobs among processing unit settings:
-nodes = 2
-procsPerNode = 1
-repsPerProc = 2
-simsPerRep = 4
-numberOfJobs = [simsPerRep, simsPerRep*repsPerProc*procsPerNode*nodes]
-
-
-################################################################################
 # Main function: 
 ################################################################################
 
-# Set up saving directories
-saveResultDir = 'savedResults'
-outputDir = 'batchSimResults'
+# Import settings:
+execfile('DDMCube_Settings.py')
+outputDir = '.batchSimResults'
+
+# Beginning computation:
+quickName = quickNamePrefix + '-' + quickNameSuffix
+numberOfJobs = [simsPerRep, simsPerRep*repsPerProc*procsPerNode*nodes]
 
 # Write a "settings" file:
 myUUID = uuid.uuid4()
@@ -79,7 +43,7 @@ settingsFileName = join(os.getcwd(), saveResultDir, quickName + '_' + str(myUUID
 pt.pickle((settings, FD, numberOfJobs, gitVersion), saveFileName = settingsFileName)
 
 # Display settings:
-at.printSettings(quickName, saveResultDir)
+at.printSettings(quickName, saveResultDir = saveResultDir)
 
 # Run the job:
 tBegin = time.mktime(time.localtime())
