500e2b252e7baa529dc0e6d706f6762a36580595
Nicholas Cain <nicain.seattle@gmail.com>
2010-05-05 14:19:09 -0700

    debug

--- recoverLostJob.py
+++ recoverLostJob.py
@@ -20,10 +20,24 @@ print '  From directory: ' + str(outputDir)
 # Get settings from the environment:
 quickNameSuffix = os.environ['JOBLOCATION']
 saveResultDir = 'savedResults-' + quickNameSuffix
+if quickNameSuffix == 'Booboo' or localRun == 1:
+	user='nicain'
+	runLocation = 'local'
+elif quickNameSuffix == 'Abe':
+	user='ncain'
+	runLocation = 'abe'
+elif quickNameSuffix == 'Steele':
+	user='cainn'
+	runLocation = 'steele'
+quickName = quickNamePrefix + '-' + quickNameSuffix
 
 # Grab the name of the settings for the run:
 print os.path.join(os.getcwd(),saveResultDir,settingsFile)
 settings, FD, numberOfJobs, gitVersion = pt.unpickle(os.path.join(os.getcwd(),saveResultDir,settingsFile))
+print settings
+
+# Push necessary settings out of settings dictionary:
+quickName = settings['quickName']
 
 # Save the day:
 resultList = pt.getFromPickleJar(loadDir = outputDir, fileNameSubString = 'simResults.dat')
